74330f3409f472d8aae38490d8d3b336d56d8041
Mahmud babu <mahmudbabu@admins-Mac-mini.fios-router.home>
2020-08-11 00:05:34 -0400

    Updates

---
+++ precache-manifest.d5b52a4a5355bcb2fc218f2a2adbd624.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e7e496d9401d30b9ddd28c4fd168b1",
+    "url": "/rsp/index.html"
+  },
+  {
+    "revision": "9822ace0c7dbe776f630",
+    "url": "/rsp/static/css/main.480396e6.chunk.css"
+  },
+  {
+    "revision": "6332d960d17fd253a606",
+    "url": "/rsp/static/js/2.8d9f6112.chunk.js"
+  },
+  {
+    "revision": "1d549b11cb9a0d87f2345e055e3cada2",
+    "url": "/rsp/static/js/2.8d9f6112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9822ace0c7dbe776f630",
+    "url": "/rsp/static/js/main.37629b97.chunk.js"
+  },
+  {
+    "revision": "60d362c5f8bb9f6e96ae",
+    "url": "/rsp/static/js/runtime-main.a4281cc3.js"
+  }
+]);
